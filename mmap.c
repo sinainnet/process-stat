836ec0a5ee859921588c21f29c99afdd469a0dd5
sinainnet <sinainnet@gmail.com>
2021-07-04 09:12:21 +0430

    read state based on the kernel task_mem reporting model

--- mmap.c
+++ mmap.c
@@ -8,10 +8,31 @@
 #include <linux/slab.h>
 #include <asm/uaccess.h>
 #include <linux/pid.h>
+#include <linux/sched/mm.h>
 
 #define MAX_SIZE (PAGE_SIZE * 10)   /* max size mmaped to userspace */
 #define DEVICE_NAME "mchar"
 #define  CLASS_NAME "mogu"
+
+struct memory_stat_report
+{
+	unsigned long vmpeak;
+	unsigned long vmsize;
+	unsigned long vmlck;
+	unsigned long vmpin;
+	unsigned long vmhwm;
+	unsigned long vmrss;
+	unsigned long rssanon;
+	unsigned long rssfile;
+	unsigned long rssshmem;
+	unsigned long vmdata;
+	unsigned long vmstk;
+	unsigned long vmexe;
+	unsigned long vmlib;
+	unsigned long vmpte;
+	unsigned long vmswap;
+	unsigned long hugetlb;
+};
 
 static struct class*  class;
 static struct device*  device;
@@ -80,23 +101,75 @@ out:
 static ssize_t mchar_read(struct file *filep, char *buffer, size_t len, loff_t *offset)
 {
     int ret;
+
+	struct mm_struct* given_task_mm_struct = NULL;
+
+	size_t mm_struct_size = 0;
     
+	if (given_task)
+	{
+		given_task_mm_struct = get_task_mm(given_task);
+		if (given_task_mm_struct)
+		{
+			mm_struct_size = sizeof(*given_task_mm_struct);
+			{
+				struct mm_struct* mm = given_task_mm_struct;
+				unsigned long text, lib, swap, anon, file, shmem;
+				unsigned long hiwater_vm, total_vm, hiwater_rss, total_rss;
+				anon = get_mm_counter(mm, MM_ANONPAGES);
+				file = get_mm_counter(mm, MM_FILEPAGES);
+				shmem = get_mm_counter(mm, MM_SHMEMPAGES);
+				hiwater_vm = total_vm = mm->total_vm;
+				if (hiwater_vm < mm->hiwater_vm)
+					hiwater_vm = mm->hiwater_vm;
+				hiwater_rss = total_rss = anon + file + shmem;
+				if (hiwater_rss < mm->hiwater_rss)
+					hiwater_rss = mm->hiwater_rss;
+
+				text = (PAGE_ALIGN(mm->end_code) - (mm->start_code & PAGE_MASK)) >> 10;
+				lib = (mm->exec_vm << (PAGE_SHIFT-10)) - text;
+				swap = get_mm_counter(mm, MM_SWAPENTS);
+				if (sh_mem)
+				{
+					struct memory_stat_report* msr = (struct memory_stat_report*)sh_mem;
+					msr->vmpeak = hiwater_vm << (PAGE_SHIFT-10);
+					msr->vmsize = total_vm << (PAGE_SHIFT-10);
+					msr->vmlck = mm->locked_vm << (PAGE_SHIFT-10);
+					msr->vmpin = mm->pinned_vm << (PAGE_SHIFT-10);
+					msr->vmhwm = hiwater_rss << (PAGE_SHIFT-10);
+					msr->vmrss = total_rss << (PAGE_SHIFT-10);
+					msr->rssanon = anon << (PAGE_SHIFT-10);
+					msr->rssfile = file << (PAGE_SHIFT-10);
+					msr->rssshmem = shmem << (PAGE_SHIFT-10);
+					msr->vmdata = mm->data_vm << (PAGE_SHIFT-10);
+					msr->vmstk = mm->stack_vm << (PAGE_SHIFT-10);
+					msr->vmexe = text;
+					msr->vmlib = lib;
+					msr->vmpte = mm_pgtables_bytes(mm) >> 10;
+					msr->vmswap = swap << (PAGE_SHIFT-10);
+					msr->hugetlb = atomic_long_read(&mm->hugetlb_usage) << (PAGE_SHIFT - 10);
+				}
+			}
+			mmput(given_task_mm_struct);
+		}
+	}
+
     if (len > MAX_SIZE) {
         pr_info("read overflow!\n");
         ret = -EFAULT;
         goto out;
     }
 
-	if (len < sizeof(given_task_pid))
+	if (len < sizeof(mm_struct_size))
 	{
         pr_info("read overflow!\n");
         ret = -EFAULT;
         goto out;
     }
 
-    if (copy_to_user(buffer, &given_task_pid, sizeof(given_task_pid)) == 0) {
+    if (copy_to_user(buffer, &mm_struct_size, sizeof(mm_struct_size)) == 0) {
         pr_info("mchar: copy %u char to the user\n", len);
-        ret = sizeof(given_task_pid);
+        ret = sizeof(mm_struct_size);
     } else {
         ret =  -EFAULT;   
     } 
--- test.c
+++ test.c
@@ -6,6 +6,27 @@
 #include<errno.h>
   
 #define DEVICE_FILENAME "/dev/mchar"  
+
+struct memory_stat_report
+{
+	unsigned long vmpeak;
+	unsigned long vmsize;
+	unsigned long vmlck;
+	unsigned long vmpin;
+	unsigned long vmhwm;
+	unsigned long vmrss;
+	unsigned long rssanon;
+	unsigned long rssfile;
+	unsigned long rssshmem;
+	unsigned long vmdata;
+	unsigned long vmstk;
+	unsigned long vmexe;
+	unsigned long vmlib;
+	unsigned long vmpte;
+	unsigned long vmswap;
+	unsigned long hugetlb;
+};
+
   
 int main()  
 {  
@@ -15,21 +36,7 @@ int main()
     char buff[64];    
   
     fd = open(DEVICE_FILENAME, O_RDWR|O_NDELAY);  
-  
-    if(fd >= 0) { 
-        p = (char*)mmap(0,  
-                4096,  
-                PROT_READ | PROT_WRITE,  
-                MAP_SHARED,  
-                fd,  
-                0);  
-        printf("%s", p);  
-        munmap(p, 4096);  
-  
-        close(fd);  
-    }  
 
-    fd = open(DEVICE_FILENAME, O_RDWR|O_NDELAY);
 	int pid = getpid();
 	ret = write(fd, (char*)(&pid), sizeof(pid));
     if (ret < 0) {
@@ -38,15 +45,63 @@ int main()
         goto out;
     }
 
-    ret = read(fd, buff, 4);
+    ret = read(fd, buff, sizeof(size_t));
     if (ret < 0) {
         printf("read error!\n");
         ret = errno;
         goto out;
     }
-
 	printf("mypid is: %d\n", getpid());
-    printf("read: %d\n", *((int*)(&buff[0])));
+    printf("read: %ld\n", *((size_t*)(&buff[0])));
+  
+    if(fd >= 0) { 
+        p = (char*)mmap(0,  
+                4096,  
+                PROT_READ | PROT_WRITE,  
+                MAP_SHARED,  
+                fd,  
+                0);  
+		struct memory_stat_report* msr = (struct memory_stat_report*)p;
+		printf("VmPeak:\t%8lu kB\n" \
+			"VmSize:\t%8lu kB\n" \
+			"VmLck:\t%8lu kB\n" \
+			"VmPin:\t%8lu kB\n" \
+			"VmHWM:\t%8lu kB\n" \
+			"VmRSS:\t%8lu kB\n" \
+			"RssAnon:\t%8lu kB\n" \
+			"RssFile:\t%8lu kB\n" \
+			"RssShmem:\t%8lu kB\n" \
+			"VmData:\t%8lu kB\n" \
+			"VmStk:\t%8lu kB\n" \
+			"VmExe:\t%8lu kB\n" \
+			"VmLib:\t%8lu kB\n" \
+			"VmPTE:\t%8lu kB\n" \
+			"VmSwap:\t%8lu kB\n" \
+			"HugetlbPages:\t%8lu kB\n",
+			msr->vmpeak,
+			msr->vmsize,
+			msr->vmlck,
+			msr->vmpin,
+			msr->vmhwm,
+			msr->vmrss,
+			msr->rssanon,
+			msr->rssfile,
+			msr->rssshmem,
+			msr->vmdata,
+			msr->vmstk,
+			msr->vmexe,
+			msr->vmlib,
+			msr->vmpte,
+			msr->vmswap,
+			msr->hugetlb);
+        printf("%s", p);  
+        munmap(p, 4096);  
+  
+        close(fd);  
+    }  
+
+    //fd = open(DEVICE_FILENAME, O_RDWR|O_NDELAY);
+
 out:
     return ret;  
 }  
